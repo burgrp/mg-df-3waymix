fbe7e83b48f3e1b57a03acc9dfa1d4f5a9d59c6b
Pavel Burgr <pb@drake.cz>
2018-09-03 20:48:16 +0200

    WIP

--- fs/init.js
+++ fs/init.js
@@ -21,6 +21,17 @@ GPIO.set_mode(ledPin, GPIO.MODE_OUTPUT);
 let pcfAddress = Cfg.get("mix.pcf");
 print("PCF8574 address:", pcfAddress);
 
+let pcf = PCF8574.create(pcfAddress, i2c);
+let pcfOut = [];
+
+function updatePcf() {
+    let v = 0;
+    for (let c = 0; c < 8; c++) {
+        v |= (pcfOut[c]? 1: 0) << c;
+    }
+    pcf.write((~v) & 0xFF);
+}
+
 let gpos = [];
 
 for (let c = 0; c < 2; c++) {
@@ -52,7 +63,6 @@ for (let c = 0; c < 2; c++) {
             root.mix["channel" + JSON.stringify(this.mix.index)] = {
                 target: v
             };
-            print(JSON.stringify(root));
             return root;
         })),
         pump: Register.add("pump" + cStr, RegisterVariable.create(true)),
@@ -60,15 +70,61 @@ for (let c = 0; c < 2; c++) {
         max: Cfg.get("mix.channel" + cStr + ".max"),
         outCcw: Cfg.get("mix.channel" + cStr + ".ccw"),
         outCw: Cfg.get("mix.channel" + cStr + ".cw"),
+        outPump: Cfg.get("mix.channel" + cStr + ".pump"),
         runMsC: Cfg.get("mix.channel" + cStr + ".runMsC"),
+        runMaxSec: Cfg.get("mix.channel" + cStr + ".runMaxSec"),
         waitSec: Cfg.get("mix.channel" + cStr + ".waitSec"),
         blocked: false,
         check: function() {
-            print("Updating mix", c, this.actual.value, this.target.value, this.enabled.value, this.blocked, this.max, this.outCcw, this.outCw, this.pump.value);
-            if (this.enabled.value && this.pump.value && this.actual.value !== null && !this.blocked) {
-                let runMs = (this.target.value - this.actual.value) * this.runMsC;
-                print("Run ms:", runMs);
-            }
+            //print("Updating mix", c, this.actual.value, this.target.value, this.enabled.value, this.blocked, this.max, this.outCcw, this.outCw, this.pump.value);           
+
+            pcfOut[this.outPump] = this.enabled.value && this.pump.value;
+
+            if (
+                this.enabled.value &&
+                this.pump.value && 
+                this.target.value !== null && 
+                this.actual.value !== null && 
+                !this.blocked
+            ) {
+                
+                let target = this.target.value;
+                if (target > this.max) {
+                    print("Target of mix", c, "oveflows maximum", this.max);
+                    target = this.max;
+                }
+
+                let runMs = (target - this.actual.value) * this.runMsC;
+                let dir = runMs > 0;
+                runMs = Math.abs(runMs);
+                
+                if (runMs > 500) {
+
+                    if (runMs > this.runMaxSec * 1000) {
+                        runMs = this.runMaxSec * 1000;
+                    }
+
+                    print("Running mix", c, dir? "CW": "CCW", "for", runMs, "ms");
+                    this.blocked = true;
+                    pcfOut[this.outCcw] = !dir;
+                    pcfOut[this.outCw] = dir;
+                    updatePcf();
+
+                    Timer.set(runMs, false, function(mix) {
+                        
+                        pcfOut[mix.outCcw] = false;
+                        pcfOut[mix.outCw] = false;
+                        updatePcf();
+                        print("Mix", mix.index, "stopped, will wait", mix.waitSec, "seconds");
+                        
+                        Timer.set(mix.waitSec * 1000, false, function(mix) {
+                            mix.blocked = false;
+                            print("End of wait of mix", mix.index);
+                        }, mix);
+
+                    }, this);
+                }
+            }                        
         }
     };
 
@@ -76,23 +132,20 @@ for (let c = 0; c < 2; c++) {
     mixs[c].target.mix = mixs[c];
 }
 
-let pcf = PCF8574.create(pcfAddress, i2c);
 
 Timer.set(tickMs, true, function (ctx) {
 
     GPIO.write(ledPin, 1);
 
-    let pcfOut = 0;
-
     for (let c = 0; c < gpos.length; c++) {
-        pcfOut |= gpos[c].register.value? 1 << gpos[c].channel: 0;
+        pcfOut[gpos[c].channel] = gpos[c].register.value? 1: 0;
     }
 
     for (let c = 0; c < mixs.length; c++) {
         mixs[c].check();
-    }
-    
-    pcf.write((~pcfOut) & 0xFF);
+    }    
+
+    updatePcf();
 
     Timer.set(100, false, function() {
         GPIO.write(ledPin, 0);
